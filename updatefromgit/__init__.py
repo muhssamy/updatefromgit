b67abd9b8b572ecd190df48925980009674d2900
Muhammad Samy <azzam.ma@nahdi.sa>
2024-10-16 14:03:09 +0300

    fix missed functions

--- updatefromgit/__init__.py
+++ updatefromgit/__init__.py
@@ -6,6 +6,7 @@ from .gitfunctions import (
     acquire_token_user_id_password_confidential,
     acquire_token_user_id_password_public,
     commit_all_items_to_git,
+    generate_config_file,
     get_git_status,
     update_workspace_from_git,
 )
